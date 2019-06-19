1602ba028e9e2516dfaa1dd7e7a33f4cc7f61a63
harishkumar <harishkumar8826@gmail.com>
2019-06-19 19:01:41 +0530

    intitial commimt

--- app/src/main/java/com/tiffinsystem/NavigationActivity.java
+++ app/src/main/java/com/tiffinsystem/NavigationActivity.java
@@ -1,5 +1,7 @@
 package com.tiffinsystem;
 
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -13,7 +15,9 @@ import androidx.appcompat.app.ActionBarDrawerToggle;
 import android.view.MenuItem;
 
 import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.tabs.TabLayout;
 import com.tiffinsystem.adapter.BannerImgAdapter;
+import com.tiffinsystem.adapter.PageAdapter;
 import com.tiffinsystem.fragment.AddressFragment;
 import com.tiffinsystem.fragment.ContactUsFragment;
 import com.tiffinsystem.fragment.HomeFragment;
@@ -30,6 +34,7 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager.widget.ViewPager;
 
 import android.view.Menu;
 import android.widget.FrameLayout;
@@ -61,6 +66,8 @@ public class NavigationActivity extends AppCompatActivity
     NavigationView navigationView;
     FrameLayout frameLayout;
     Fragment fragment;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
 
 
     @Override
@@ -69,6 +76,16 @@ public class NavigationActivity extends AppCompatActivity
         setContentView(R.layout.activity_navigation);
         setId();
         setSupportActionBar(toolbar);
+        /* adding the tabLayout*/
+        addTabDynamically();
+        setUpTabIcons();
+        /*adding the listener for the tabLayout*/
+        listenerOnTab();
+        /*adding the listener for the viewpager*/
+        listenerOnViewPager();
+        /*adding the adapter on viewpager*/
+        PageAdapter pageAdapter = new PageAdapter(getSupportFragmentManager(), tabLayout.getTabCount());
+        viewPager.setAdapter(pageAdapter);
        // CallBannerApi();
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
@@ -77,6 +94,46 @@ public class NavigationActivity extends AppCompatActivity
         displaySelectedScreen(R.id.nav_home);
         navigationView.setNavigationItemSelectedListener(this);
         
+    }
+
+    private void listenerOnViewPager() {
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int i, float v, int i1) {
+
+            }
+
+            @Override
+            public void onPageSelected(int i) {
+                /* if (tabLayout.getTabAt(i) != null) {*/
+                tabLayout.getTabAt(i).select();
+                /* }*/
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int i) {
+
+            }
+        });
+    }
+    private void setUpTabIcons() {
+        int tabIcons[] = {
+                R.drawable.restaurant,
+                R.drawable.restaurant,
+                R.drawable.ic_search,
+                R.drawable.ic_shopping_cart
+        };
+        tabLayout.getTabAt(0).setIcon(tabIcons[0]);
+        tabLayout.getTabAt(1).setIcon(tabIcons[1]);
+        tabLayout.getTabAt(2).setIcon(tabIcons[2]);
+        tabLayout.getTabAt(3).setIcon(tabIcons[3]);
+    }
+    private void addTabDynamically() {
+        tabLayout.addTab(tabLayout.newTab().setText("Restaurant"));
+        tabLayout.addTab(tabLayout.newTab().setText("Tiffin"));
+        tabLayout.addTab(tabLayout.newTab().setText("Search"));
+        tabLayout.addTab(tabLayout.newTab().setText("Cart"));
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
     }
 
     private void displaySelectedScreen(int itemViewId) {
@@ -119,6 +176,8 @@ public class NavigationActivity extends AppCompatActivity
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
         api = retrofit.create(NetworkClient.class);
+        viewPager = findViewById(R.id.viewpager_restaurant);
+        tabLayout = findViewById(R.id.tabLayout);
     }
 
     @Override
@@ -157,6 +216,62 @@ public class NavigationActivity extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);*/
         return true;
     }
+    private void listenerOnTab() {
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+                int tabPosition = tab.getPosition();
+                switch (tabPosition)
+                {
+                    case 0:
+                        tabLayout.getTabAt(0).getIcon().setColorFilter(Color.parseColor("#DD2C00"), PorterDuff.Mode.SRC_IN);
+                        break;
+                    case 1:
+                        tabLayout.getTabAt(1).getIcon().setColorFilter(Color.parseColor("#DD2C00"), PorterDuff.Mode.SRC_IN);
+                        break;
+                    case 2:
+                        tabLayout.getTabAt(2).getIcon().setColorFilter(Color.parseColor("#DD2C00"), PorterDuff.Mode.SRC_IN);
+                        break;
+                    case 3:
+                        tabLayout.getTabAt(3).getIcon().setColorFilter(Color.parseColor("#DD2C00"), PorterDuff.Mode.SRC_IN);
+                        break;
+                    default:
+                        break;
 
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+                int tabPosition = tab.getPosition();
+                switch (tabPosition)
+                {
+                    case 0:
+                        tabLayout.getTabAt(0).getIcon().setColorFilter(Color.parseColor("#757575"), PorterDuff.Mode.SRC_IN);
+                        break;
+                    case 1:
+                        tabLayout.getTabAt(1).getIcon().setColorFilter(Color.parseColor("#757575"), PorterDuff.Mode.SRC_IN);
+                        break;
+                    case 2:
+                        tabLayout.getTabAt(2).getIcon().setColorFilter(Color.parseColor("#757575"), PorterDuff.Mode.SRC_IN);
+                        break;
+                    case 3:
+                        tabLayout.getTabAt(3).getIcon().setColorFilter(Color.parseColor("#757575"), PorterDuff.Mode.SRC_IN);
+                        break;
+                    case 4:
+                        tabLayout.getTabAt(4).getIcon().setColorFilter(Color.parseColor("#757575"), PorterDuff.Mode.SRC_IN);
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+    }
 
 }
--- app/src/main/java/com/tiffinsystem/network/NetworkClient.java
+++ app/src/main/java/com/tiffinsystem/network/NetworkClient.java
@@ -8,7 +8,7 @@ import retrofit2.http.Query;
 public interface NetworkClient {
     String BASE_URL1 = "https://newsapi.org/v2/";
     //String BASE_URL2 = "http://192.168.100.204/broccoli_api/api/user/";
-    String BASE_URL2 = "http://demo.ledsolarcart.com/broccoli_api/api/user/";
+    String BASE_URL2 = "http://demo.ledsolarcart.com/food/broccoli_api/api/user/";
     @GET("login.php")
     Call<ResponseBody> getOtpNumber(@Query("phone") String phoneNumber);
     @GET("slider_images.php")
