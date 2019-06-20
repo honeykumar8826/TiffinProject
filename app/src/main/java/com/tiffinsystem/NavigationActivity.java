5a64bd3c92fd23e7f5c1461d4d342063ceeb34d9
harishkumar <harishkumar8826@gmail.com>
2019-06-20 13:54:04 +0530

    intial commit

--- app/src/main/java/com/tiffinsystem/NavigationActivity.java
+++ app/src/main/java/com/tiffinsystem/NavigationActivity.java
@@ -1,7 +1,9 @@
 package com.tiffinsystem;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
+import android.net.Uri;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -19,10 +21,13 @@ import com.google.android.material.tabs.TabLayout;
 import com.tiffinsystem.adapter.BannerImgAdapter;
 import com.tiffinsystem.adapter.PageAdapter;
 import com.tiffinsystem.fragment.AddressFragment;
+import com.tiffinsystem.fragment.CartFragment;
 import com.tiffinsystem.fragment.ContactUsFragment;
 import com.tiffinsystem.fragment.HomeFragment;
 import com.tiffinsystem.fragment.OrderFragment;
 import com.tiffinsystem.fragment.ProfileFragment;
+import com.tiffinsystem.fragment.SearchFragment;
+import com.tiffinsystem.fragment.TiffinFragment;
 import com.tiffinsystem.modal.BannerImgModal;
 import com.tiffinsystem.network.NetworkClient;
 
@@ -68,6 +73,9 @@ public class NavigationActivity extends AppCompatActivity
     Fragment fragment;
     private TabLayout tabLayout;
     private ViewPager viewPager;
+    FragmentTransaction fragmentTransaction;
+    Boolean isTabSelected=false;
+    Boolean isNavSelected = false;
 
 
     @Override
@@ -81,12 +89,6 @@ public class NavigationActivity extends AppCompatActivity
         setUpTabIcons();
         /*adding the listener for the tabLayout*/
         listenerOnTab();
-        /*adding the listener for the viewpager*/
-        listenerOnViewPager();
-        /*adding the adapter on viewpager*/
-        PageAdapter pageAdapter = new PageAdapter(getSupportFragmentManager(), tabLayout.getTabCount());
-        viewPager.setAdapter(pageAdapter);
-       // CallBannerApi();
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
@@ -96,30 +98,10 @@ public class NavigationActivity extends AppCompatActivity
         
     }
 
-    private void listenerOnViewPager() {
-        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
-            @Override
-            public void onPageScrolled(int i, float v, int i1) {
-
-            }
-
-            @Override
-            public void onPageSelected(int i) {
-                /* if (tabLayout.getTabAt(i) != null) {*/
-                tabLayout.getTabAt(i).select();
-                /* }*/
-            }
-
-            @Override
-            public void onPageScrollStateChanged(int i) {
-
-            }
-        });
-    }
     private void setUpTabIcons() {
         int tabIcons[] = {
                 R.drawable.restaurant,
-                R.drawable.restaurant,
+                R.drawable.lunchbox,
                 R.drawable.ic_search,
                 R.drawable.ic_shopping_cart
         };
@@ -141,31 +123,121 @@ public class NavigationActivity extends AppCompatActivity
         {
             case R.id.nav_home:
                 fragment = new HomeFragment();
+                tabLayout.setVisibility(View.VISIBLE);
+
                 break;
             case R.id.nav_profile:
+                isNavSelected = true;
+                tabLayout.setVisibility(View.GONE);
                 fragment = new ProfileFragment();
                 break;
             case R.id.nav_orders:
+                tabLayout.setVisibility(View.GONE);
                 fragment = new OrderFragment();
                 break;
             case R.id.nav_address:
+                tabLayout.setVisibility(View.GONE);
                 fragment = new AddressFragment();
                 break;
             case R.id.nav_contactus:
-                fragment = new ContactUsFragment();
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:9999999999"));
+                startActivity(intent);
+                /*tabLayout.setVisibility(View.GONE);
+                fragment = new ContactUsFragment();*/
                 break;
                 default:
                     break;
         }
         //replacing the fragment
         if (fragment != null) {
-            FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
-            ft.replace(R.id.fl_container, fragment);
-            ft.commit();
+            fragmentTransaction = getSupportFragmentManager().beginTransaction();
+            fragmentTransaction.replace(R.id.fl_container, fragment);
+            fragmentTransaction.commit();
         }
         drawer.closeDrawer(GravityCompat.START);
     }
+    private void listenerOnTab() {
+        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+                int tabPosition = tab.getPosition();
+                // for doing tablayout position on a first index when we select the navigation item
+             /*   if(isNavSelected)
+                {
+                    tabPosition = 0;
+                }*/
+                switch (tabPosition)
+                {
+                    case 0:
+                        fragment = new HomeFragment();
+                        toolbar.setTitle("Home");
+                       // isNavSelected = false;
+                        tabLayout.getTabAt(0).getIcon().setColorFilter(Color.parseColor("#DD2C00"), PorterDuff.Mode.SRC_IN);
+                        break;
+                    case 1:
+                        toolbar.setTitle("Tiffin");
+                        //isNavSelected = false;
+                        fragment = new TiffinFragment();
+                        tabLayout.getTabAt(1).getIcon().setColorFilter(Color.parseColor("#DD2C00"), PorterDuff.Mode.SRC_IN);
+                        break;
+                    case 2:
+                        fragment = new SearchFragment();
+                        toolbar.setTitle("Search");
+                       // isNavSelected = false;
+                        tabLayout.getTabAt(2).getIcon().setColorFilter(Color.parseColor("#DD2C00"), PorterDuff.Mode.SRC_IN);
+                        break;
+                    case 3:
+                        fragment = new CartFragment();
+                        toolbar.setTitle("Cart");
+                       // isNavSelected = false;
+                        tabLayout.getTabAt(3).getIcon().setColorFilter(Color.parseColor("#DD2C00"), PorterDuff.Mode.SRC_IN);
+                        break;
+                    default:
+                        break;
 
+                }
+
+                //replacing the fragment
+                if (fragment != null) {
+                     fragmentTransaction = getSupportFragmentManager().beginTransaction();
+                    fragmentTransaction.replace(R.id.fl_container, fragment);
+                    fragmentTransaction.commit();
+                }
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+                int tabPosition = tab.getPosition();
+                switch (tabPosition)
+                {
+                    case 0:
+                        tabLayout.getTabAt(0).getIcon().setColorFilter(Color.parseColor("#757575"), PorterDuff.Mode.SRC_IN);
+                        break;
+                    case 1:
+                        tabLayout.getTabAt(1).getIcon().setColorFilter(Color.parseColor("#757575"), PorterDuff.Mode.SRC_IN);
+                        break;
+                    case 2:
+                        tabLayout.getTabAt(2).getIcon().setColorFilter(Color.parseColor("#757575"), PorterDuff.Mode.SRC_IN);
+                        break;
+                    case 3:
+                        tabLayout.getTabAt(3).getIcon().setColorFilter(Color.parseColor("#757575"), PorterDuff.Mode.SRC_IN);
+                        break;
+                    case 4:
+                        tabLayout.getTabAt(4).getIcon().setColorFilter(Color.parseColor("#757575"), PorterDuff.Mode.SRC_IN);
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+    }
     private void setId() {
         toolbar = findViewById(R.id.toolbar);
         drawer  = findViewById(R.id.drawer_layout);
@@ -216,62 +288,6 @@ public class NavigationActivity extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);*/
         return true;
     }
-    private void listenerOnTab() {
-        tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
-            @Override
-            public void onTabSelected(TabLayout.Tab tab) {
-                viewPager.setCurrentItem(tab.getPosition());
-                int tabPosition = tab.getPosition();
-                switch (tabPosition)
-                {
-                    case 0:
-                        tabLayout.getTabAt(0).getIcon().setColorFilter(Color.parseColor("#DD2C00"), PorterDuff.Mode.SRC_IN);
-                        break;
-                    case 1:
-                        tabLayout.getTabAt(1).getIcon().setColorFilter(Color.parseColor("#DD2C00"), PorterDuff.Mode.SRC_IN);
-                        break;
-                    case 2:
-                        tabLayout.getTabAt(2).getIcon().setColorFilter(Color.parseColor("#DD2C00"), PorterDuff.Mode.SRC_IN);
-                        break;
-                    case 3:
-                        tabLayout.getTabAt(3).getIcon().setColorFilter(Color.parseColor("#DD2C00"), PorterDuff.Mode.SRC_IN);
-                        break;
-                    default:
-                        break;
 
-                }
-            }
-
-            @Override
-            public void onTabUnselected(TabLayout.Tab tab) {
-                int tabPosition = tab.getPosition();
-                switch (tabPosition)
-                {
-                    case 0:
-                        tabLayout.getTabAt(0).getIcon().setColorFilter(Color.parseColor("#757575"), PorterDuff.Mode.SRC_IN);
-                        break;
-                    case 1:
-                        tabLayout.getTabAt(1).getIcon().setColorFilter(Color.parseColor("#757575"), PorterDuff.Mode.SRC_IN);
-                        break;
-                    case 2:
-                        tabLayout.getTabAt(2).getIcon().setColorFilter(Color.parseColor("#757575"), PorterDuff.Mode.SRC_IN);
-                        break;
-                    case 3:
-                        tabLayout.getTabAt(3).getIcon().setColorFilter(Color.parseColor("#757575"), PorterDuff.Mode.SRC_IN);
-                        break;
-                    case 4:
-                        tabLayout.getTabAt(4).getIcon().setColorFilter(Color.parseColor("#757575"), PorterDuff.Mode.SRC_IN);
-                        break;
-                    default:
-                        break;
-                }
-            }
-
-            @Override
-            public void onTabReselected(TabLayout.Tab tab) {
-
-            }
-        });
-    }
 
 }
--- app/src/main/java/com/tiffinsystem/fragment/ProfileFragment.java
+++ app/src/main/java/com/tiffinsystem/fragment/ProfileFragment.java
@@ -1,16 +1,24 @@
 package com.tiffinsystem.fragment;
 
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.view.ContextThemeWrapper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.tiffinsystem.R;
 import com.tiffinsystem.preferencemanager.PreferenceStorage;
@@ -21,6 +29,7 @@ import com.tiffinsystem.preferencemanager.PreferenceStorage;
 public class ProfileFragment extends Fragment {
 
     TextView userName, userEmail, userPhone;
+    ImageView imgName, imgEmail;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -42,13 +51,57 @@ public class ProfileFragment extends Fragment {
         userName = view.findViewById(R.id.tv_name);
         userEmail = view.findViewById(R.id.tv_email);
         userPhone = view.findViewById(R.id.tv_phone);
+        imgName = view.findViewById(R.id.img_edit_name);
+        imgEmail = view.findViewById(R.id.img_edit_email);
         setUserRecord();
+        imgName.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                updateUserProile();
+            }
+        });
+        imgEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                updateUserProile();
+
+            }
+        });
+    }
+
+    private void updateUserProile() {
+        LayoutInflater inflater = getLayoutInflater();
+        final View alertLayout = inflater.inflate(R.layout.edit_profile_layout, null);
+        final EditText etUsername = alertLayout.findViewById(R.id.et_name);
+        final EditText etAge = alertLayout.findViewById(R.id.et_email);
+        final Button btnCancel = alertLayout.findViewById(R.id.btn_cancel);
+        final Button btnOk = alertLayout.findViewById(R.id.btn_ok);
+        final AlertDialog.Builder alert = new AlertDialog.Builder(new ContextThemeWrapper(getActivity(),R.style.AlertBuilderTheme));
+        btnOk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = etUsername.getText().toString();
+                String email = etAge.getText().toString();
+
+                Toast.makeText(getActivity(), "data Stored Successfully", Toast.LENGTH_SHORT).show();
+            }
+        });
+        btnCancel.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                alert.setCancelable(true);
+            }
+        });
+        // this is set the view from XML inside AlertDialog
+        alert.setView(alertLayout);
+        alert.show();
     }
 
     private void setUserRecord() {
         PreferenceStorage preferenceStorage = new PreferenceStorage(getActivity());
-
         String phoneNumber = preferenceStorage.getMobileNumber();
         userPhone.setText(phoneNumber);
     }
+
+
 }
